e872a5385f3d051d37cb53344e5c76d7be2a54f5
Rasmus Beck <rbb@e-conomic.com>
2015-11-12 01:11:44 +0100

    Added java example X-EconomicAppIdentifier to java example.

--- examples/java/src/main/java/com/economic/example/SoapClient/SoapClient.java
+++ examples/java/src/main/java/com/economic/example/SoapClient/SoapClient.java
@@ -9,6 +9,9 @@ import javax.xml.ws.BindingProvider;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Collections;
 
 public class SoapClient {
     public static void main(String[] args) {
@@ -25,15 +28,32 @@ public class SoapClient {
 
         EconomicWebService service = new EconomicWebService(apiUrl);
         EconomicWebServiceSoap session = service.getEconomicWebServiceSoap();
+        BindingProvider bindingProvider = ((BindingProvider)session);
 
-        ((BindingProvider)session).getRequestContext().put(BindingProvider.SESSION_MAINTAIN_PROPERTY,true);
+        //Enable session cookies
+        bindingProvider.getRequestContext().put(BindingProvider.SESSION_MAINTAIN_PROPERTY,true);
+
+        //Add X-EconomicAppIdentifier header
+        Map<String, List<String>> headers = new HashMap<String, List<String>>();
+        headers.put("X-EconomicAppIdentifier", Collections.singletonList("X-EconomicAppIdentifier: MyCoolIntegration/1.1 (http://example.com/MyCoolIntegration/; MyCoolIntegration@example.com) BasedOnSuperLib/1.4"));
+        bindingProvider.getRequestContext().put("javax.xml.ws.http.request.headers", headers);
 
         try {
             session.connect(agreementNumber, userName, password);
         } catch (Exception e) {
-            System.out.println("Connection error! " + e);
+            System.out.println("Connection error! \n" + e);
+            return;
         }
         System.out.println("Connection successful!");
+
+        try {
+            session.verifyXEconomicAppIdentifier();
+        } catch (Exception e) {
+            System.out.println("X-EconomicAppIdentifier not valid! \n" + e);
+            return;
+        }
+        System.out.println("X-EconomicAppIdentifier is valid!");
+
 
 
         ArrayOfOrderHandle orders = session.orderGetAll();
